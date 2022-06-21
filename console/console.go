4d2df5e45b5badea98569da8015e5523eaf03f87
Umaxyon <umaxyon@gmail.com>
2022-06-21 14:03:23 +0900

    console refactering and print current disc

--- console/console.go
+++ console/console.go
@@ -5,17 +5,43 @@ import (
 	"fmt"
 )
 
-func Print(b board.Board) {
+const (
+	BlackDisc = "x"
+	WhiteDisc = "o"
+)
+
+type Console struct {
+	board board.Board
+}
+
+func NewConsole(b board.Board) *Console {
+	return &Console{board: b}
+}
+
+func (c *Console) DrawCurrentDisc() string {
+	var disc string
+	switch c.board.CurrentColor {
+	case board.Black:
+		disc = BlackDisc
+		break
+	case board.White:
+		disc = WhiteDisc
+		break
+	}
+	return disc
+}
+
+func (c *Console) Print() {
 	fmt.Println("  a b c d e f g h ")
 	for y := 1; y <= 8; y++ {
 		fmt.Printf("%d", y)
 		for x := 1; x <= 8; x++ {
-			switch b.GetColor(*board.NewPoint(x, y)) {
+			switch c.board.GetColor(*board.NewPoint(x, y)) {
 			case board.Black:
-				fmt.Print(" x")
+				fmt.Printf(" %s", BlackDisc)
 				break
 			case board.White:
-				fmt.Print(" o")
+				fmt.Printf(" %s", WhiteDisc)
 				break
 			default:
 				fmt.Print("  ")
@@ -23,5 +49,44 @@ func Print(b board.Board) {
 			}
 		}
 		fmt.Println()
+	}
+}
+
+func (c *Console) Start() {
+	for true {
+		b := &c.board
+		c.Print()
+		fmt.Println()
+		fmt.Printf("Black %d  White %d  Empty %d",
+			b.CountDisc(board.Black), b.CountDisc(board.White), b.CountDisc(board.Empty))
+		fmt.Println()
+
+		if b.IsGameOver() {
+			fmt.Println("--------Game Over!!---------")
+			return
+		}
+
+		var in string
+		fmt.Printf("Input[%s] (p: pass, u: undo): ", c.DrawCurrentDisc())
+		fmt.Scan(&in)
+
+		if in == "p" {
+			if !b.Pass() {
+				fmt.Print("\nCan't Pass!!\n\n")
+			}
+			continue
+		}
+
+		if in == "u" {
+			b.Undo()
+			continue
+		}
+
+		p := board.PointIs(in)
+
+		if !b.Move(*p) {
+			fmt.Print("\nCan't put there!!\n\n")
+			continue
+		}
 	}
 }
--- main.go
+++ main.go
@@ -3,45 +3,9 @@ package main
 import (
 	"Osero/board"
 	"Osero/console"
-	"fmt"
 )
 
 func main() {
 	b := board.NewBoard()
-
-	for true {
-		console.Print(*b)
-		fmt.Println()
-		fmt.Printf("Black %d  White %d  Empty %d",
-			b.CountDisc(board.Black), b.CountDisc(board.White), b.CountDisc(board.Empty))
-		fmt.Println()
-
-		if b.IsGameOver() {
-			fmt.Println("--------Game Over!!---------")
-			return
-		}
-
-		var in string
-		fmt.Print("Input (p: pass, u: undo): ")
-		fmt.Scan(&in)
-
-		if in == "p" {
-			if !b.Pass() {
-				fmt.Print("\nCan't Pass!!\n\n")
-			}
-			continue
-		}
-
-		if in == "u" {
-			b.Undo()
-			continue
-		}
-
-		p := board.PointIs(in)
-
-		if !b.Move(*p) {
-			fmt.Print("\nCan't put there!!\n\n")
-			continue
-		}
-	}
+	console.NewConsole(*b).Start()
 }
